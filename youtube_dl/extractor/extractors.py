1e9000bcf1631db2b32acc2a678b9d195246887e
TRox1972 <TRox1972@users.noreply.github.com>
2016-05-21 17:48:17 +0200

    [vidio] Add extractor (Closes #7195)
    [Vidio] fix fallback value and wrap duration in int_or_none
    
    [Vidio] don't use video_id for _html_search_regex()

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -912,6 +912,7 @@ from .videomore import (
 )
 from .videopremium import VideoPremiumIE
 from .videott import VideoTtIE
+from .vidio import VidioIE
 from .vidme import (
     VidmeIE,
     VidmeUserIE,
